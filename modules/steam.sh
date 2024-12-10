732fa34a93d41ac8a77252569c714d727e02e7d4
PolyCat <contact.polycat@proton.me>
2024-12-10 10:52:40 +0200

    added OTD

--- modules/steam.sh
+++ modules/steam.sh
@@ -3,9 +3,14 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+
 rpm-ostree install\
+    ./OpenTabletDriver.rpm\
     steam\
     gamescope\
     mangohud
+
+rm -f ./OpenTabletDriver.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

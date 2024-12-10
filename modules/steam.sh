b9fcfbcac1e1a622971375762043f112494d0065
PolyCat <contact.polycat@proton.me>
2024-12-10 11:06:11 +0200

    refacored OTD installer

--- modules/steam.sh
+++ modules/steam.sh
@@ -4,13 +4,13 @@
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
 curl -o otd.rpm https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+rpm-ostree install ./otd.rpm
+rm -f ./otd.rpm
 
 rpm-ostree install\
-    ./otd.rpm\
     steam\
     gamescope\
     mangohud
 
-rm -f ./otd.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

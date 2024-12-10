8ed20eccdaeabf6df9306d36061704aa1704ab2c
PolyCat <contact.polycat@proton.me>
2024-12-10 12:20:59 +0200

    readded OTD

--- modules/steam.sh
+++ modules/steam.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-#wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
-#rpm-ostree install\
-#    ./OpenTabletDriver.rpm\
-#    dotnet-runtime-8.0
-
-#rm -f ./OpenTabletDriver.rpm
+rpm-ostree install dotnet-runtime-8.0
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
 
 rpm-ostree install\
+    ./OpenTabletDriver.rpm\
     steam\
     gamescope\
     mangohud
-
+    
+rm -f ./OpenTabletDriver.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

5ca4cc22d9613704d48695472b48deb88edfd1bc
PolyCat <contact.polycat@proton.me>
2024-12-10 11:20:57 +0200

    added dotnet runtime 8

--- modules/steam.sh
+++ modules/steam.sh
@@ -4,7 +4,10 @@
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
-rpm-ostree install ./OpenTabletDriver.rpm
+rpm-ostree install\
+    ./OpenTabletDriver.rpm\
+    dotnet-runtime-8.0
+
 rm -f ./OpenTabletDriver.rpm
 
 rpm-ostree install\

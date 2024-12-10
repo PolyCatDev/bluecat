12bf7cc922e570eeec989a212666f2799336f606
PolyCat <contact.polycat@proton.me>
2024-12-10 11:34:55 +0200

    commented out OTD

--- modules/steam.sh
+++ modules/steam.sh
@@ -3,12 +3,12 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
-rpm-ostree install\
-    ./OpenTabletDriver.rpm\
-    dotnet-runtime-8.0
+#wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+#rpm-ostree install\
+#    ./OpenTabletDriver.rpm\
+#    dotnet-runtime-8.0
 
-rm -f ./OpenTabletDriver.rpm
+#rm -f ./OpenTabletDriver.rpm
 
 rpm-ostree install\
     steam\

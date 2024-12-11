07cf5328eb56eafb71a0d7e72853a50b2d003ee2
PolyCat <contact.polycat@proton.me>
2024-12-11 11:24:02 +0200

    fixed syntax error

--- modules/steam.sh
+++ modules/steam.sh
@@ -3,9 +3,9 @@
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
 #wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+#   ./OpenTabletDriver.rpm\
 
 rpm-ostree install\
-#   ./OpenTabletDriver.rpm\
     steam\
     gamescope\
     mangohud

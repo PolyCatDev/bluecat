05cb0b3a3a33c10ec1ac454c900e8809c53aa6d8
PolyCat <contact.polycat@proton.me>
2024-12-13 18:40:47 +0200

    made dotnet 8 apply libe

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,14 +2,17 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
+rpm-ostree install --apply-live\
+    dotnet-runtime-8.0
+
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+
 rpm-ostree install\
-    dotnet-runtime-8.0\
+    OpenTabletDriver.rpm\
     steam\
     gamescope\
     mangohud
     
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
-rpm-ostree install --ignore-missing-dependencies OpenTabletDriver.rpm
 rm -f ./OpenTabletDriver.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

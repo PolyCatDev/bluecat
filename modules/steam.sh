445dbaba1e30ef7929c39d884894a3ea15fc6857
PolyCat <contact.polycat@proton.me>
2024-12-16 17:30:48 +0200

    removed otd

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,28 +2,28 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-rpm-ostree install --apply-live\
-    dotnet-runtime-8.0\
-    dotnet-sdk-8.0 \
-    libevdev \
-    gtk3
+#rpm-ostree install --apply-live\
+#    dotnet-runtime-8.0\
+#    dotnet-sdk-8.0 \
+#    libevdev \
+#    gtk3
 
 
-export DOTNET_ROOT="/tmp/.dotnet"
-mkdir -p "$DOTNET_ROOT"
-export PATH="$DOTNET_ROOT:$PATH"
+#export DOTNET_ROOT="/tmp/.dotnet"
+#mkdir -p "$DOTNET_ROOT"
+#export PATH="$DOTNET_ROOT:$PATH"
 
-git clone https://github.com/OpenTabletDriver/OpenTabletDriver.git --branch 0.6.x
-cd OpenTabletDriver
-source ./eng/linux/package.sh --package RedHat --output ./dist/redhat
+#git clone https://github.com/OpenTabletDriver/OpenTabletDriver.git --branch 0.6.x
+#cd OpenTabletDriver
+#source ./eng/linux/package.sh --package RedHat --output ./dist/redhat
+#    ./dist/redhat/OpenTabletDriver.rpm\
 
 rpm-ostree install\
-    ./dist/redhat/OpenTabletDriver.rpm\
     steam\
     gamescope\
     mangohud
 
-cd ..
-rm -rf ./OpenTabletDriver
+#cd ..
+#rm -rf ./OpenTabletDriver
 
 echo -e "\e[32m[End Steam Module]\e[0m"

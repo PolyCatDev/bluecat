1a1fe4fda049b16225edc423a7b98bb2c7055ad2
PolyCat <contact.polycat@proton.me>
2024-12-16 17:22:51 +0200

    removed depricated deps

--- modules/steam.sh
+++ modules/steam.sh
@@ -5,8 +5,6 @@ echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 rpm-ostree install --apply-live\
     dotnet-runtime-8.0\
     dotnet-sdk-8.0 \
-    libx11 \
-    libxrandr \
     libevdev \
     gtk3
 

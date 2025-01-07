43abffba6407096ddb35f9075a011ac5ea6fd241
PolyCat <contact.polycat@proton.me>
2025-01-07 19:08:18 +0200

    added OpenTabletDriver

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,28 +2,14 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-#rpm-ostree install --apply-live\
-#    dotnet-runtime-8.0\
-#    dotnet-sdk-8.0 \
-#    libevdev \
-#    gtk3
-
-
-#export DOTNET_ROOT="/tmp/.dotnet"
-#mkdir -p "$DOTNET_ROOT"
-#export PATH="$DOTNET_ROOT:$PATH"
-
-#git clone https://github.com/OpenTabletDriver/OpenTabletDriver.git --branch 0.6.x
-#cd OpenTabletDriver
-#source ./eng/linux/package.sh --package RedHat --output ./dist/redhat
-#    ./dist/redhat/OpenTabletDriver.rpm\
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
 
 rpm-ostree install\
     steam\
     gamescope\
-    mangohud
+    mangohud\
+    ./OpenTabletDriver.rpm
 
-#cd ..
-#rm -rf ./OpenTabletDriver
+rm -f ./OpenTabletDriver.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

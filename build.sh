0cb36f7908354408a6e60af0638ec2226f9b99ca
PolyCat <contact.polycat@proton.me>
2025-01-18 12:40:55 +0200

    stopped akmods module

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 cd /tmp
 
 source ./modules/core.sh
-source ./modules/akmods.sh
+#source ./modules/akmods.sh
 source ./modules/gnome-extensions.sh
 source ./modules/dev-tools.sh
 source ./modules/steam.sh

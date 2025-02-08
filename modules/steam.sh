32e92572c7cf278ca50dd424c1485241e641910e
PolyCat <contact.polycat@proton.me>
2025-02-08 09:51:25 +0200

    updated otd download link

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm
+curl -o OpenTabletDriver.rpm https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/opentabletdriver-0.6.5.1-1.x86_64.rpm
 
 rpm-ostree install\
     gamescope\

ba93f33cae8f8e8baf3674b7518cbd3636aa3464
PolyCat <contact.polycat@proton.me>
2025-02-08 10:34:08 +0200

    implemented web scraper to get the latest otd version link automatically

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,13 +2,17 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-curl -L -o OpenTabletDriver.rpm https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/opentabletdriver-0.6.5.1-1.x86_64.rpm
+curl -L -o get-otd-link.gz https://github.com/PolyCatDev/bluecat/raw/refs/heads/main/scripts/get-otd-link/get-otd-link.gz
+gunzip get-otd-link.gz
+chmod +x get-otd-link
+curl -L -o otd.rpm $(./get-otd-link)
+rm -f ./get-otd-link
 
 rpm-ostree install\
     gamescope\
     mangohud\
-    ./OpenTabletDriver.rpm
+    ./otd.rpm
 
-rm -f ./OpenTabletDriver.rpm
+rm -f ./otd.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

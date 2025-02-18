dc3b7b6b9877d0476074fd95948c8158add31da5
PolyCat <contact.polycat@proton.me>
2025-02-18 11:28:26 +0200

    enabled otd

--- modules/steam.sh
+++ modules/steam.sh
@@ -2,17 +2,17 @@
 
 echo -e "\e[38;5;214m[Start Steam Module]\e[0m"
 
-#curl -L -o get-otd-link.gz https://github.com/PolyCatDev/bluecat/raw/refs/heads/main/scripts/get-otd-link/get-otd-link.gz
-#gunzip get-otd-link.gz
-#chmod +x get-otd-link
-#curl -L -o otd.rpm $(./get-otd-link)
-#rm -f ./get-otd-link
+curl -L -o get-otd-link.gz https://github.com/PolyCatDev/bluecat/raw/refs/heads/main/scripts/get-otd-link/get-otd-link.gz
+gunzip get-otd-link.gz
+chmod +x get-otd-link
+curl -L -o otd.rpm $(./get-otd-link)
+rm -f ./get-otd-link
 
 rpm-ostree install\
     gamescope\
     mangohud\
- #   ./otd.rpm
+    ./otd.rpm
 
-#rm -f ./otd.rpm
+rm -f ./otd.rpm
 
 echo -e "\e[32m[End Steam Module]\e[0m"

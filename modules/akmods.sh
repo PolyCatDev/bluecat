a32fc055b8acec8c661600c05a018a0eb1077605
PolyCat <contact.polycat@proton.me>
2025-02-19 10:13:56 +0200

    testing new akmods method

--- modules/akmods.sh
+++ modules/akmods.sh
@@ -2,9 +2,23 @@
 
 echo -e "\e[38;5;214m[Start AKMods Module]\e[0m"
 
-rpm-ostree install\
-    v4l2loopback\
-    kernel-devel\
-    kernel-headers
+# Install required packages
+dnf install -y \
+    akmods-nvidia \
+    kernel-modules-extra \
+    && dnf clean all
+
+# Download and copy the signing key
+curl -s -o /tmp/akmods-ublue.der \
+    https://github.com/ublue-os/akmods/raw/main/certs/public_key.der
+mkdir -p /etc/pki/akmods/certs/
+cp /tmp/akmods-ublue.der /etc/pki/akmods/certs/
+
+# Configure module to load at boot
+mkdir -p /etc/modules-load.d/
+echo v4l2loopback > /etc/modules-load.d/v4l2loopback.conf
+
+# Set up module signing for Secure Boot
+mokutil --import /etc/pki/akmods/certs/akmods-ublue.der
 
 echo -e "\e[32m[End AKMods Module]\e[0m"

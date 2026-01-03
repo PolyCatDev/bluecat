91599ac54145bbc4dfb293401faebe96d88cf9a0
PolyCat <contact.polycat@proton.me>
2026-01-03 21:55:13 +0200

    Changed akmods install script to dnf5

--- files/scripts/install-akmods.sh
+++ files/scripts/install-akmods.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 find /tmp/rpms
-rpm-ostree install /tmp/rpms/kmods/kmod-v4l2loopback*.rpm
+dnf5 install -y /tmp/rpms/kmods/kmod-v4l2loopback*.rpm
